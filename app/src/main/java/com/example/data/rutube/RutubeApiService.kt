ad93765a3fc5469fe20ed795ee9212a41a8b747c
ByteBudda <lxdeveaux@gmail.com>
2026-06-02 23:29:18 +0300

    Update RutubeApiService.kt

--- app/src/main/java/com/example/data/rutube/RutubeApiService.kt
+++ app/src/main/java/com/example/data/rutube/RutubeApiService.kt
@@ -1,29 +1,23 @@
 package com.example.data.rutube
 
-import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface RutubeApiService {
-    // Поиск видео с пагинацией
     @GET("api/search/video/")
     suspend fun searchVideos(
         @Query("query") query: String,
         @Query("page") page: Int = 1,
         @Query("format") format: String = "json"
-    ): ResponseBody
+    ): RutubeSearchResponse
 
-    // Популярные видео с пагинацией
     @GET("api/feeds/popular/")
     suspend fun getPopularVideos(
         @Query("page") page: Int = 1,
         @Query("format") format: String = "json"
-    ): ResponseBody
+    ): RutubeSearchResponse
 
-    // Универсальный метод для любого URL (в том числе для next)
     @GET
-    suspend fun getDynamicUrl(
-        @Url url: String
-    ): ResponseBody
+    suspend fun getDynamicUrl(@Url url: String): RutubeSearchResponse
 }
