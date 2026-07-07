f10fa5f68258bf8cd02a5f36fffd20cabedcd2f0
ByteBudda <lxdeveaux@gmail.com>
2026-07-07 14:42:00 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 3
-    versionName = "3.5.60"
+    versionName = "3.5.61"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
