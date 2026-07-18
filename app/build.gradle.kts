d71dcf6355d2b1949f3cd0481c8bf07ff0f6b8b0
ByteBudda <205501055+ByteBudda@users.noreply.github.com>
2026-07-18 23:06:04 +0300

    Change applicationId from com.aistudio to com.bytebudda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.ruvideohub.zmpqyq"
+    applicationId = "com.bytebudda.ruvideohub"
     minSdk = 24
     targetSdk = 36
     versionCode = 10
